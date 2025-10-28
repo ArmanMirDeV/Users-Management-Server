b569d9d504d2c753d25008fa68045cb944644076
Arman Mir <mirarman8583@gmail.com>
2025-10-28 16:56:11 +0600

    server Created

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const express = require('express');
 
+const app = express;
+
+const port = process.env.PORT || 3000;
+
+app.length('/', (req, res) => {
+    res.send("Users Server Is Available")
+})
+
+app.listen(port, () => {
+    console.log(`Users Server started on port: ${port}`);
+    
+})
